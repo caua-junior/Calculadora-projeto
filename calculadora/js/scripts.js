6a07f7207600355aff70c9126e4fafed31e83a51
cauan-junior <vitincaua@gmail.com>
2024-08-21 13:03:00 +0100

    Update scripts.js

--- calculadora/js/scripts.js
+++ calculadora/js/scripts.js
@@ -4,19 +4,20 @@ const buttons = document.querySelectorAll("#buttons-container button")
 
 class Calculator {
     constructor(previousOperationText,currentOperationText){
-        this.previousOperationText = previousOperationText
-        this.currentOperationText = currentOperationText
-        this.currentOperation = ""
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
     }
 
     //add digit to calculator screen
     addDigit(digit){
         //check if current operation already has a dot
+        console.log(digit);
         if (digit === "." && this.currentOperationText.innerText.includes(".")){
             return;
         }
-       this.currentOperation = digit
-       this.updateScreen()
+       this.currentOperation = digit;
+       this.updateScreen();
     }
 
 //Process all calculator operations
@@ -33,8 +34,8 @@ processOperation(operation){
 
 //Get current and previous value
 let operationValue;
-const previous = +this.previousOperationText.innerText.split(" ")[0];
-const current = +this.currentOperationText.innerText;
+let previous = +this.previousOperationText.innerText.split(" ")[0];
+let current = +this.currentOperationText.innerText;
 
 switch (operation) {
     case "+":
@@ -64,8 +65,6 @@ switch (operation) {
         break;
     case "=":
         this.processEqualOperator();
-        this.currentOperationText.innerHTML = this.previousOperationText.innerHTML.split('')[0];
-        this.previousOperationText.innerHTML = "";
         break;
     default:
         return;
@@ -101,12 +100,13 @@ switch (operation) {
 
         //change math operation
         changeOperation(operation){
-            const mathOperation = ["*","/","-","+"]
-            if (!mathOperation.includes(operation)){
+            const mathOperations = [ "*" , "/" , "-" , "+" ]
+            if (!mathOperations.includes(operation)){
                 return;
             }
 
-            this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0,-1) + operation;
+            this.previousOperationText.innerText =
+             this.previousOperationText.innerText.slice(0,-1) + operation;
         }
         //Delete the last digit
         processDelOperator(){
@@ -123,8 +123,7 @@ switch (operation) {
         }
         //Process an operation
         processEqualOperator(){
-            //this.currentOperationText.innerText = "esse usuario ja existe"
-            const operation = previousOperationText.innerText.split(" ")[1]
+            let operation = previousOperationText.innerText.split(" ")[1];
               this.processOperation(operation)
         }
     }
@@ -137,9 +136,10 @@ buttons.forEach((btn) => {
         const value = e.target.innerText;
 
         if(+value >= 0 || value === "."){
-            calc.addDigit(value)
+            console.log(value);
+            calc.addDigit(value);
         }else{
-            calc.processOperation(value)
+            calc.processOperation(value);
         }
-    })
-})
+    });
+});
